027a14763f90a592d234d815c80722f5a3b5b4e1
scorp7mix <scorp7mix@gmail.com>
2015-06-08 12:16:03 +0300

    hw 8 fixed

--- dz_8/js/notes.js
+++ dz_8/js/notes.js
@@ -1,13 +1,18 @@
 var ls = localStorage;
 var notes = $('#notes');
-var noNotesMsg = $('<div>Записей нет</div>');
-var notesArr, noteId, noteText, newNote, maxId;
+var notesArr,       // массив заметок из localStorage
+    noteId,         // id текущей заметки
+    noteText,       // текст текущей заметки
+    newNote,        // объект новой заметки
+    maxId;          // максимальный id
 
+// обновление списка заметок
 function refreshNotes() {
     notes.empty();
     if(ls.length == 0) {
-        notes.append(noNotesMsg);
+        notes.append($('<div>Записей нет</div>'));
     } else {
+        // сначала заметки выгружаются в массив, для сортировки
         notesArr = [];
         for(var i = 1, l = ls.length; i < l + 1; i++) {
             notesArr.push([Number(ls.key(i - 1)), ls.getItem(ls.key(i - 1))]);
@@ -15,6 +20,7 @@ function refreshNotes() {
         notesArr.sort(function(a,b){return b[0]-a[0]});
         maxId = 0;
 
+        // потом выводятся в подготовленный div
         for(i = 0, l = notesArr.length; i < l; i++) {
             noteId = notesArr[i][0];
             maxId = noteId > maxId ? noteId : maxId;
@@ -23,11 +29,16 @@ function refreshNotes() {
             newNote.append($('<div class="note-id" hidden>' + noteId + '</div>'));
             newNote.append($('<div class="col-xs-11 note-text">' + noteText + '</div>'));
             newNote.append($('<div class="col-xs-1"><button type="button" class="close note-del">&times;</button></div>'));
-            //if(i != l) newNote.append($('<div class="col-xs-12"><hr></div>'));
             notes.append(newNote);
         }
         noteId = false;
     }
+    // просмотр выбранной заметки
+    $('.note').on('click', function() {
+        noteId = $(this).find('.note-id').text();
+        $('#note-edit-text').val(ls.getItem(String(noteId))).prop('disabled','');
+    });
+    // удаление заметки
     $('.note-del').on('click', function() {
         ls.removeItem($(this).parent().parent().find('.note-id').text());
         refreshNotes();
@@ -38,6 +49,7 @@ $(document).ready(function() {
 
     refreshNotes();
 
+    // сохранение редактированной заметки
     $('#note-btn-save').on('click', function() {
         if(!noteId) {
             noteId = maxId + 1;
@@ -49,10 +61,12 @@ $(document).ready(function() {
         refreshNotes();
     });
 
+    // создание новой заметки
     $('#note-btn-new').on('click', function() {
         $('#note-edit-text').val('').prop('disabled','');
     });
 
+    // разблокирование кнопки "сохранить" если текст не пустой
     $('#note-edit-text').on('input', function() {
         if($('#note-edit-text').val().trim() == '') {
             $('#note-btn-save').prop('disabled','disabled');
